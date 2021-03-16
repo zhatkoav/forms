da6483d9e6c7a07a13f4c0685f4221e23a308a0c
zhatkoav <medtaxi@yandex.ru>
2021-03-16 22:03:20 +0300

    first drying
    произвел первую оптимизацию кода

--- form-3.py
+++ form-3.py
@@ -12,55 +12,16 @@ import os, sys
 # In[2]:
 
 
-result = pd.DataFrame()
+src_files = os.scandir('src_forms3\\src_csv')
+output_path = 'src_forms4\\csv\\'
+pool_of_filds = pd.read_csv('src_forms3\\raw\\forms_raw6.csv', dtype=str) #исходый файл предварительно орботан вручную
 
 
 # In[3]:
 
 
-src_df = {}
-src_files = os.scandir('src_forms')
-
-
-# In[4]:
-
-
-src_files2 = os.scandir('src_forms3/2')
-
-
-# In[5]:
-
-
-filds = pd.read_csv('src_forms3/raw/forms_raw6.csv', dtype=str)
-
-
-# for i in src_files:
-#     result = pd.DataFrame()
-#     result['unitkey'] = pd.read_csv(i.path, dtype=str).dropna(how='all').apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
-#     result = pd.merge(result, filds, how='left', on='unitkey')
-#     result = result[['name', 'type', 'label', 'value', 'options']]
-#     file_name = 'src_forms3\\' + result.name[result.type=='divider'] + '.csv'
-#     #result.to_csv(file_name[0], index=False)
-#     print(file_name[0])
-
-# In[6]:
-
-
-for i in src_files:
-    result = pd.DataFrame()
-    result['unitkey'] = pd.read_csv(i.path, dtype=str).dropna(how='all').apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
-    result = pd.merge(result, filds, how='left', on='unitkey')
-    result = result[['name', 'type', 'label', 'value', 'options']]
-    file_name = 'src_forms4\\csv\\' + result['name'][result['type']=='divider'] + '.csv'
-    result.to_csv(file_name[0], index=False)
-
-
-# In[7]:
-
-
-def paramtounit(x):
+def create_unit_key(x):
     x = x.dropna()
-    #print(x['label'])
     if 'label' in x and 'value' in x:
         return ','.join([x['label'], x['value'], 'Нет'])
     elif 'label' in x and 'options' in x:
@@ -69,21 +30,22 @@ def paramtounit(x):
             x['options'][y] = x['options'][y].split('=')[0]
         x['options'] = ','.join(x['options'])
         return ','.join([x['label'], x['options']])
-    else :
+    elif 'label' in x:
         return x['label']
+    else :
+        return ','.join(x.dropna().astype(str))
 
 
-# In[8]:
+# In[4]:
 
 
-for i in src_files2:
+for i in src_files:
     result = pd.DataFrame()
-    result = pd.read_csv(i.path, dtype=str)
-    result['unitkey'] = result.apply(lambda x: paramtounit(x) ,axis=1)
-    result = pd.merge(result['unitkey'], filds, how='left', on='unitkey')
+    result['unitkey'] = pd.read_csv(i.path, dtype=str).dropna(how='all').apply(lambda x: create_unit_key(x) ,axis=1)
+    result = pd.merge(result, pool_of_filds, how='left', on='unitkey')
     result = result[['name', 'type', 'label', 'value', 'options']]
-    file_name = 'src_forms4\\csv\\' + result['name'][result['type']=='divider'] + '.csv'
-    result.to_csv(file_name[0], index=False)
+    file_name = output_path + result['name'][result['type']=='divider'].iloc[0] + '.csv'
+    result.to_csv(file_name, index=False)
 
 
 # In[ ]:
